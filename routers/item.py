1b5e7a4d6b9d8ff8f67407850b04c3f5a46e0e2c
oshiro <kaito.o.0628@gmail.com>
2024-02-28 21:15:30 +0900

    データの更新と削除処理の実装

--- routers/item.py
+++ routers/item.py
@@ -29,16 +29,16 @@ async def find_by_name(db: DbDependency, name: str = Query(min_length=2, max_len
 async def create(db: DbDependency, item_create: ItemCreate):
   return item_cruds.create(db, item_create)
 
-# @router.put("/{id}", response_model=ItemResponse, status_code=status.HTTP_200_OK)
-# async def update(item_update: ItemUpdate, id: int = Path(gt=0)):
-#   updated_item = item_cruds.update(id, item_update)
-#   if not updated_item:
-#     raise HTTPException(status_code=404, detail="Item not updated")
-#   return updated_item
+@router.put("/{id}", response_model=ItemResponse, status_code=status.HTTP_200_OK)
+async def update(db: DbDependency, item_update: ItemUpdate, id: int = Path(gt=0)):
+  updated_item = item_cruds.update(db, id, item_update)
+  if not updated_item:
+    raise HTTPException(status_code=404, detail="Item not updated")
+  return updated_item
 
-# @router.delete("/{id}", response_model=ItemResponse, status_code=status.HTTP_200_OK)
-# async def delete(id: int = Path(gt=0)):
-#   deleted_item = item_cruds.delete(id)
-#   if not deleted_item:
-#     raise HTTPException(status_code=404, detail="Item not deleted")
-#   return deleted_item
+@router.delete("/{id}", response_model=ItemResponse, status_code=status.HTTP_200_OK)
+async def delete(db: DbDependency, id: int = Path(gt=0)):
+  deleted_item = item_cruds.delete(db, id)
+  if not deleted_item:
+    raise HTTPException(status_code=404, detail="Item not deleted")
+  return deleted_item
