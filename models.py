9c121f18266d228578397467c41b6e517d6e9123
oshiro <kaito.o.0628@gmail.com>
2024-03-02 14:52:52 +0900

    パスワードのハッシュ化

--- models.py
+++ models.py
@@ -20,5 +20,6 @@ class User(Base):
   id = Column(Integer, primary_key=True)
   username = Column(String, nullable=False, unique=True)
   password = Column(String, nullable=False)
+  salt = Column(String, nullable=False)
   created_at = Column(DateTime, default=datetime.now())
   update_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
