644e88d29f793b8d090fc09394818a005ed567f9
oshiro <kaito.o.0628@gmail.com>
2024-03-03 10:56:01 +0900

    JWTトークンの設定

--- cruds/auth.py
+++ cruds/auth.py
@@ -2,13 +2,18 @@ from datetime import datetime, timedelta
 import hashlib
 import base64
 import os
-from jose import jwt
+from typing import Annotated
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
 from sqlalchemy.orm import Session
-from schemas import UserCreate
+from schemas import UserCreate, DecodedToken
 from models import User
 
 ALGORITHM = "HS256"
 SECRET_KEY = "ac4b7b1db5377c3144e90e4c314b77080063a65732c10a92d11f9804036bd21d"
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def create_user(db: Session, user_create: UserCreate):
   salt = base64.b64encode(os.urandom(32))
@@ -42,3 +47,15 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
   payload = {"sub": username, "id": user_id, "exp": expires}
 
   return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+  try:
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    username = payload.get("sub")
+    user_id = payload.get("id")
+    if username is None or user_id is None:
+      return None
+
+    return DecodedToken(username=username, user_id=user_id)
+  except JWTError:
+    raise JWTError
--- schemas.py
+++ schemas.py
@@ -44,3 +44,7 @@ class UserResponse(BaseModel):
 class Token(BaseModel):
   access_token: str
   token_type: str
+
+class DecodedToken(BaseModel):
+  username: str
+  user_id: int
